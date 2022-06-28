cceada02fddf37f9b40ace84d441dabb91b13658
Jfontaine <jfontaine19@gmail.com>
2022-06-28 14:25:25 -0400

    final

---
+++ markov.js
@@ -0,0 +1,42 @@
+class MarkovMachine {
+
+    /** build markov machine; read in text.*/
+  
+    constructor(text) {
+      let words = text.split(/[ \r\n]+/);
+      this.words = words.filter(c => c !== "");
+      this.makeChains();
+    }
+  
+    /** set markov chains:
+     *
+     *  for text of "the cat in the hat", chains will be
+     *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+  
+    makeChains() {
+      // TODO
+      let chains = new Map();
+
+      for (let i = 0; i < this.words.length; i += 1) {
+        let word = this.words[i];
+        let nextWord = this.words[i + 1] || null;
+  
+        if (chains.has(word)) chains.get(word).push(nextWord);
+        else chains.set(word, [nextWord]);
+      }
+  
+      this.chains = chains;
+    }
+  
+  
+    /** return random text from chains */
+    const random = ((array) => {
+        return array[Math.floor(Math.random() * array.length)];
+      })
+    
+  
+    makeText(numWords = 100) {
+      // TODO
+     
+    }
+  }
